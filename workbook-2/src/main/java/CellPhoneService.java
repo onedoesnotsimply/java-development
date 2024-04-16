6b246443e8d01320802e6b10cf6e0312169a2dc5
Emily Trifone <emilytrifone@gmail.com>
2024-04-16 13:48:22 -0400

    Completed workbook-2 - Section 2 - Excercise 2

--- workbook-2/src/main/java/CellPhoneService.java
+++ workbook-2/src/main/java/CellPhoneService.java
@@ -1,12 +1,28 @@
 import java.util.Scanner;
 
 public class CellPhoneService {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        CellPhone phone = new CellPhone();
-        Scanner scanner = new Scanner(System.in);
+        CellPhone phone1 = new CellPhone();
+        CellPhone phone2 = new CellPhone();
 
-        // Prompt for and update phone object info
+        // Prompt users for information on their phones
+        promptInfo(phone1);
+        promptInfo(phone2);
+
+        // Display the phone properties
+        display(phone1);
+        display(phone2);
+
+        // Dial other phone
+        phone1.dial(phone2.phoneNumber);
+        phone2.dial(phone1.phoneNumber);
+
+    }
+
+    // Populates CellPhone objects
+    public static void promptInfo(CellPhone phone) {
         System.out.print("What is the serial number? ");
         phone.setSerialNumber(scanner.nextInt());
         scanner.nextLine();
@@ -23,18 +39,20 @@ public class CellPhoneService {
         System.out.print("Who is the owner of the phone? ");
         phone.setOwner(scanner.nextLine());
         System.out.println(" ");
+    }
 
-        // Print out the phone properties
-        System.out.println("Cell Phone Properties:");
+    // Displays CellPhone values
+    public static void display(CellPhone phone) {
+        System.out.println("Cell Phone Properties");
         System.out.println("Serial Number: "+phone.getSerialNumber());
         System.out.println("Model: "+phone.getModel());
         System.out.println("Carrier: "+phone.getCarrier());
         System.out.println("Phone Number: "+phone.getPhoneNumber());
         System.out.println("Owner: "+phone.getOwner());
-
-
+        System.out.println(" ");
     }
 
+    // CellPhone Class
     public static class CellPhone {
         private int serialNumber;
         private String model;
@@ -92,6 +110,14 @@ public class CellPhoneService {
 
         public void setOwner(String owner) {
             this.owner = owner;
+        }
+
+        // Methods
+        // Dials a phone number
+        public void dial(String phoneNumber){
+            System.out.printf("%s's phone is calling %s",this.owner, phoneNumber);
+            System.out.println(" ");
+
         }
     }
 }
