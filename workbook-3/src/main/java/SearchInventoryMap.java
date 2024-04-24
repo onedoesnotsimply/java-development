6e9e892e5850f01d98e47353213f7b8227fca6d7
Emily Trifone <emilytrifone@gmail.com>
2024-04-24 15:02:16 -0400

    Completed workbook-3 Section 4 Exercise 1

---
+++ workbook-3/src/main/java/SearchInventoryMap.java
@@ -0,0 +1,101 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SearchInventoryMap {
+    // Initialize the HashMap
+    static HashMap<Integer, Product> inventory = new HashMap<Integer, Product>();
+
+    public static void main(String[] args) {
+        // Load the items into the HashMap
+        readCSV();
+
+        // Create a scanner object for prompting
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for the item id
+        System.out.print("What item # are you interested in? ");
+        int id = scanner.nextInt();
+        Product matchedProduct = inventory.get(id);
+
+        // If .get() returns null print an error message and quit the program
+        if (matchedProduct == null){
+            System.out.println("We don't carry that product");
+            scanner.close();
+            return;
+        }
+
+        // Print the item and close the scanner object
+        System.out.printf("We carry %s and the price is $%.2f", matchedProduct.getName(),matchedProduct.getPrice());
+        scanner.close();
+
+    }
+
+    public static void readCSV(){
+        // Make sure that the file is there
+        try{
+            // Create bfr
+            BufferedReader bfr = new BufferedReader(new FileReader("inventory.csv"));
+            // Initialize input string
+            String input;
+            boolean skippedHeader=false;
+
+            while((input = bfr.readLine()) != null){
+                if (skippedHeader == false){
+                    skippedHeader=true;
+                    continue;
+                }
+                // Sends input to a constructor method
+                loadInventory(input);
+            }
+            // Closes the buffered reader
+            bfr.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void loadInventory(String input){
+        // Create the tokens array
+        String[] tokens=input.split("\\|");
+
+        // Parse the tokens
+        int id = Integer.parseInt(tokens[0]);
+        String name = tokens[1];
+        float price = Float.parseFloat(tokens[2]);
+
+        // Create and populate the Product object
+        Product product = new Product(id,name,price);
+
+        // Add the Product to the HashMap
+        inventory.put(id, product);
+
+    }
+
+
+    public static class Product {
+        private int id;
+        private String name;
+        private float price;
+
+        public Product(int id, String name, float price){
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+        public int getId() {
+            return this.id;
+        }
+        public String getName() {
+            return this.name;
+        }
+        public float getPrice() {
+            return this.price;
+        }
+    }
+
+}
