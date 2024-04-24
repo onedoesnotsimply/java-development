a7c85a2c70db61036d6ca4047a16880fb8aa818e
Emily Trifone <emilytrifone@gmail.com>
2024-04-24 10:52:29 -0400

    Completed mod-3 exercise 3

--- workbook-3/src/main/java/SearchEngineLogger.java
+++ workbook-3/src/main/java/SearchEngineLogger.java
@@ -1,5 +1,43 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class SearchEngineLogger {
     public static void main(String[] args) {
+        // Create a scanner for prompting
+        Scanner scanner = new Scanner(System.in);
+        logAction("launch");
 
+        do {
+            // Prompt user action
+            System.out.println("Enter a search term (X to exit): ");
+            String item = scanner.nextLine();
+            if (item.equalsIgnoreCase("x")){
+                return;
+            } else {
+                logAction("search : "+item);
+            }
+
+        } while (true);
     }
+
+    public static void logAction(String action){
+        try {
+            // Create file/buffered writer
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("logs.txt", true));
+            bufferedWriter.write(getCurrentDateTime()+" "+action+"\n");
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getCurrentDateTime() {
+        LocalDateTime ldt = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return dtf.format(ldt);
+    }
+
 }
