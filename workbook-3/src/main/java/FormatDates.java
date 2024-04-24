27b20d4a5f0146271b988a0d498e0a313ea096b3
Emily Trifone <emilytrifone@gmail.com>
2024-04-24 10:08:41 -0400

    Added comments

--- workbook-3/src/main/java/FormatDates.java
+++ workbook-3/src/main/java/FormatDates.java
@@ -6,13 +6,16 @@ import java.util.Date;
 public class FormatDates {
 
     public static void main(String[] args) {
+        // Create formatters
         DateTimeFormatter backSlashFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        LocalDate todayDate = LocalDate.now();
-        LocalDateTime todayDateTime = LocalDateTime.now();
         DateTimeFormatter fullMonthFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
         DateTimeFormatter dayMonTimeFormatter = DateTimeFormatter.ofPattern("E, MMM d, yyyy  hh:mm");
 
-        // These work
+        // Create dates
+        LocalDate todayDate = LocalDate.now();
+        LocalDateTime todayDateTime = LocalDateTime.now();
+
+        // Print out formatted dates
         System.out.println(todayDate.format(backSlashFormatter)+"\n"+todayDate);
         System.out.println(todayDate.format(fullMonthFormatter));
         System.out.println(todayDateTime.format(dayMonTimeFormatter));
