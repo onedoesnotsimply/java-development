944735d34bb8dd1db5f0769e2057d05d6928f4b7
Emily Trifone <emilytrifone@gmail.com>
2024-06-11 11:37:16 -0400

    finished mod-4 exercise-1

--- workbook-8/NorthwindTraders/src/main/java/Northwind4.java
+++ workbook-8/NorthwindTraders/src/main/java/Northwind4.java
@@ -1,14 +1,27 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Northwind4 {
     public static void main(String[] args) {
+        /*
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+         */
+
+        // Create datasource
+        BasicDataSource dataSource = new BasicDataSource();
+
+        // Configure the datasource
+        dataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+
         String customerQuery = "SELECT * FROM customers ORDER BY country";
         String productQuery = "SELECT * FROM products ORDER BY productid";
         String categoryQuery = "SELECT categoryid, categoryname FROM categories ORDER BY categoryid";
@@ -16,7 +29,7 @@ public class Northwind4 {
         Scanner scanner = new Scanner(System.in);
         int choice=1;
 
-        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root","password");)
+        try(Connection connection = dataSource.getConnection())//Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root","password");)
         {
             while (choice!=0){
                 System.out.println("""
