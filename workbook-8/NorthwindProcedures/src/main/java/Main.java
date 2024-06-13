43cc3d69fa16ad8e8ca7b6f23fda569d5d61b712
Emily Trifone <emilytrifone@gmail.com>
2024-06-13 07:49:24 -0400

    finished

--- workbook-8/NorthwindProcedures/src/main/java/Main.java
+++ workbook-8/NorthwindProcedures/src/main/java/Main.java
@@ -1,6 +1,8 @@
 import com.pluralsight.DataManager;
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.time.LocalDate;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -28,27 +30,48 @@ public class Main {
                 3) Search for sales by category
                 0) Exit
                 """);
-
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-
-        if (choice==1){
-            // prompt for a customer id
-            System.out.println("Enter a customer ID");
-            String custId = scanner.nextLine();
-            dataManager.getCustomerOrderHistory(custId);
-            homeScreen();
-        } else if (choice==2) {
-            homeScreen();
-        } else if (choice==3) {
-            homeScreen();
-        } else if (choice==0) {
-            System.exit(0);
-        } else {
-            System.out.println("index out of range");
+        try{
+            int choice = scanner.nextInt();
             scanner.nextLine();
+
+            if (choice==1){
+                // prompt for a customer id
+                System.out.println("Enter a customer ID");
+                String custId = scanner.nextLine();
+
+                dataManager.getCustomerOrderHistory(custId);
+                homeScreen();
+
+            } else if (choice==2) {
+                // Prompt for a start and end date
+                System.out.println("Enter start date");
+                LocalDate startDate = LocalDate.parse(scanner.nextLine());
+                System.out.println("Enter end date");
+                LocalDate endDate = LocalDate.parse(scanner.nextLine());
+                
+                dataManager.getSalesByYear(startDate,endDate);
+                homeScreen();
+
+            } else if (choice==3) {
+                // Prompt for (string) category and year
+                System.out.println("Enter category name");
+                String category = scanner.nextLine();
+                System.out.println("Enter year");
+                int year = scanner.nextInt();
+
+                dataManager.getSalesByCategory(category,year);
+                homeScreen();
+
+            } else if (choice==0) {
+                System.exit(0);
+            } else {
+                System.out.println("index out of range");
+                scanner.nextLine();
+                homeScreen();
+            }
+        } catch (Exception e){
+            System.out.println("ERROR : incorrect input");
             homeScreen();
         }
     }
-
 }
--- workbook-8/NorthwindProcedures/src/main/java/com/pluralsight/DataManager.java
+++ workbook-8/NorthwindProcedures/src/main/java/com/pluralsight/DataManager.java
@@ -1,16 +1,76 @@
 package com.pluralsight;
 
 import javax.sql.DataSource;
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class DataManager {
     private DataSource dataSource;
 
     public DataManager(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void getSalesByCategory(String category, int year){
+        String query = "{CALL SalesByCategory (?, ?)}";
+
+        try(Connection connection = dataSource.getConnection();
+            CallableStatement callableStatement = connection.prepareCall(query);){
+
+            callableStatement.setString(1, category);
+            callableStatement.setInt(2, year);
+
+            try(ResultSet resultSet = callableStatement.executeQuery();){
+                if (resultSet.next()){
+                    do{
+                        System.out.printf("""
+                                Product Name\t%s
+                                Total Purchase\t%d
+                                ----------------------------
+                                """, resultSet.getString(1), resultSet.getInt(2));
+                    } while(resultSet.next());
+                } else {
+                    System.out.println("No sales found");
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void getSalesByYear(LocalDate startDate, LocalDate endDate){
+        String query = "{CALL `Sales by Year` (?, ?)}";
+
+        try(Connection connection = dataSource.getConnection();
+            CallableStatement callableStatement = connection.prepareCall(query);){
+
+            callableStatement.setDate(1, Date.valueOf(startDate));
+            callableStatement.setDate(2, Date.valueOf(endDate));
+
+            // Execute the query
+            try(ResultSet resultSet = callableStatement.executeQuery();){
+                // Check for null
+                if (resultSet.next()) {
+                    do {
+                        System.out.printf("""
+                                Shipped Date\t%s
+                                Order ID\t%d
+                                Subtotal\t%.2f
+                                Year\t%s
+                                ----------------------------
+                                """,resultSet.getDate(1), resultSet.getInt(2),
+                                resultSet.getDouble(3), resultSet.getDate(4));
+                    } while (resultSet.next());
+
+                } else {
+                    System.out.println("No sales found");
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void getCustomerOrderHistory(String customerId){
@@ -29,15 +89,16 @@ public class DataManager {
                 // Check for null
                 if (resultSet.next()) {
                     do {
-                        System.out.println(resultSet.getString("ProductName")+" - "+resultSet.getInt("TOTAL"));
+                        System.out.printf("""
+                                Product Name\t%s
+                                Total\t%d
+                                ----------------------------
+                                """,resultSet.getString("ProductName"),resultSet.getInt("TOTAL"));
                     } while (resultSet.next());
                 } else {
                     System.out.println("No order history found");
                 }
             }
-
-
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
